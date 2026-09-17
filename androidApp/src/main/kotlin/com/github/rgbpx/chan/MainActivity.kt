2535e59f2ee1d08e7b0af51bb02a900838275fc2
rgbpx <rgbpx@proton.me>
2026-09-17 14:57:52 +0400

    Add log messages for app start and errors

--- androidApp/src/main/kotlin/com/github/rgbpx/chan/MainActivity.kt
+++ androidApp/src/main/kotlin/com/github/rgbpx/chan/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import co.touchlab.kermit.Logger
 import com.github.rgbpx.chan.di.AppGraph
 import dev.zacsweers.metro.createGraphFactory
 
@@ -12,6 +13,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
+        Logger.withTag("App").i { "App started" }
+        
         val appGraph = createGraphFactory<AppGraph.Factory>().create(applicationContext)
 
         setContent {
